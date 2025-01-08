375374164da3b8fe91c6e2d51b61f87ed8d40924
Naegi <lucas.runser@uha.fr>
2025-01-08 23:51:51 +0100

    auto-refresh js (bugged because multiplication of the messages in chat)

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,7 @@
 // This script is used to make an AJAX request to the server to get a list of usernames that match the query entered by the user in the receiver field.
+
+let lastMessageId = null; // Variable pour suivre le dernier ID de message
+
 $(document).ready(function () {
     $('#receiver').on('input', function () {
         let query = $(this).val();
@@ -22,4 +25,22 @@ $(document).ready(function () {
         $('#receiver').val($(this).text());
         $('#suggestions-list').empty();
     });
+    
+    setInterval(function(){
+        $( ".message" ).load(window.location.href + " .message" );
+    }, 2000)
+    
 });
+
+$(document).on('submit', '#message', function(e){
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: "",
+        data: {
+            message: $('#msg').val(),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+        }
+    });
+    $( ".message-container" ).load(window.location.href + " .message-container" );
+})

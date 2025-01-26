a18f3bf19eab527d864f54473f6710ff3274fb02
Jarod <kohler.jarod.2004@gmail.com>
2025-01-26 20:36:02 +0100

    add emojis files

--- static/js/emoji-popover.js
+++ static/js/emoji-popover.js
@@ -1,2 +1,8 @@
+import 'emoji-files/emoji-activity.js';
 import 'emoji-files/emoji-flags.js';
-import 'emoji-files/emoji-person.js';
+import 'emoji-files/emoji-food.js';
+import 'emoji-files/emoji-nature.js';
+import 'emoji-files/emoji-objects.js';
+import 'emoji-files/emoji-people.js';
+import 'emoji-files/emoji-places.js';
+import 'emoji-files/emoji-symbols.js';
